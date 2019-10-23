ea4e07a88450a8041069caa43b0c6e719372e34a
Rodrigo Bastos <rodrigobastosv@gmail.com>
2019-10-23 05:09:30 -0300

    Random avatar

---
+++ lib/gg_random_circle_avatar.dart
@@ -0,0 +1,26 @@
+import 'package:avataaar_image/avataaar_image.dart';
+import 'package:flutter/material.dart';
+
+class GGRandomCircleAvatar extends StatelessWidget {
+  GGRandomCircleAvatar(
+      {this.radius, this.backgroundColor = Colors.white, this.errorImage = const Icon(Icons.error), this.placeholder = const CircularProgressIndicator()})
+      : assert(radius != null && radius > 0);
+
+  final double radius;
+  final Color backgroundColor;
+  final Widget errorImage;
+  final Widget placeholder;
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      radius: radius,
+      backgroundColor: backgroundColor,
+      child: AvataaarImage(
+        avatar: Avataaar.random(),
+        errorImage: errorImage,
+        placeholder: placeholder,
+      ),
+    );
+  }
+}
