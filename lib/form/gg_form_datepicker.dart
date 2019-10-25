482dfe2865b4317c2df8460ad5d009b6c1e055e5
Rodrigo Bastos <rodrigobastosv@gmail.com>
2019-10-24 21:06:41 -0300

    Form date picker

---
+++ lib/form/gg_form_datepicker.dart
@@ -0,0 +1,42 @@
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class GGFormDatePicker extends StatelessWidget {
+  GGFormDatePicker({
+      @required this.format,
+      this.decoration,
+      this.initialDate,
+      this.onSaved,
+      this.validator,
+  }) : assert(format != null);
+
+  final DateFormat format;
+  final InputDecoration decoration;
+  final DateTime initialDate;
+  final TextEditingController controller = TextEditingController();
+  final FormFieldSetter<DateTime> onSaved;
+  final FormFieldValidator<DateTime> validator;
+
+  @override
+  Widget build(BuildContext context) {
+    controller.text = initialDate != null ? format.format(initialDate) : '';
+    return DateTimeField(
+      decoration: decoration ?? InputDecoration(
+        prefixIcon: Icon(Icons.date_range),
+        border: OutlineInputBorder(),
+      ),
+      controller: controller,
+      onSaved: onSaved,
+      validator: validator,
+      format: format,
+      onShowPicker: (context, currentValue) {
+        return showDatePicker(
+            context: context,
+            firstDate: DateTime(1900),
+            initialDate: initialDate ?? DateTime.now(),
+            lastDate: DateTime(2100));
+      },
+    );
+  }
+}
