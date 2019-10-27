dd332a1ad5f7d97435c9cf62ccc904fdf2516e42
Rodrigo Bastos <rodrigobastosv@gmail.com>
2019-10-26 23:12:58 -0300

    Email validator

--- lib/validator/gg_validators.dart
+++ lib/validator/gg_validators.dart
@@ -1,5 +1,11 @@
+import 'package:email_validator/email_validator.dart';
+
 mixin GGValidators {
   String emptyValidator(String value, {String errorText = 'Campo Obrigatório'}) {
     return value.isEmpty ? errorText : null;
+  }
+
+  String emailValidator(String value, {String errorText = 'E-mail Inválido'}) {
+    return !EmailValidator.validate(value) ? errorText : null;
   }
 }
