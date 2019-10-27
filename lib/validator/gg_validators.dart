4aaae5a90b423317676a39ad8c8a45c0338ac3d8
Rodrigo Bastos <rodrigobastosv@gmail.com>
2019-10-26 23:41:53 -0300

    required valid email

--- lib/validator/gg_validators.dart
+++ lib/validator/gg_validators.dart
@@ -5,6 +5,18 @@ mixin GGValidators {
     return value.isEmpty ? errorText : null;
   }
 
+  String requiredEmailValidator(String value, {String errorText = 'E-mail Inválido'}) {
+    final emptyMessage = emptyValidator(value);
+    if (emptyMessage != null) {
+      return emptyMessage;
+    }
+
+    if (!EmailValidator.validate(value)) {
+      return errorText;
+    }
+    return null;
+  }
+
   String emailValidator(String value, {String errorText = 'E-mail Inválido'}) {
     return !EmailValidator.validate(value) ? errorText : null;
   }
