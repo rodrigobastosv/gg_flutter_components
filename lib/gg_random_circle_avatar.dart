e3fd93f749d24b334700b5c4fb0c7d1854b4f57b
Rodrigo Bastos <rodrigobastosv@gmail.com>
2019-10-26 22:33:56 -0300

    Improving this bitch

--- lib/gg_random_circle_avatar.dart
+++ lib/gg_random_circle_avatar.dart
@@ -1,28 +1,71 @@
 import 'package:avataaar_image/avataaar_image.dart';
 import 'package:flutter/material.dart';
 
-class GGRandomCircleAvatar extends StatelessWidget {
+enum Gender { male, female }
+
+class GGRandomCircleAvatar extends StatefulWidget {
   GGRandomCircleAvatar(
       {@required this.radius,
       this.backgroundColor = Colors.white,
       this.errorImage = const Icon(Icons.error),
+      this.gender,
       this.placeholder = const CircularProgressIndicator()})
       : assert(radius != null && radius > 0);
 
   final double radius;
   final Color backgroundColor;
   final Widget errorImage;
+  final Gender gender;
   final Widget placeholder;
+
+  @override
+  _GGRandomCircleAvatarState createState() => _GGRandomCircleAvatarState();
+}
+
+class _GGRandomCircleAvatarState extends State<GGRandomCircleAvatar> {
+  Avataaar avatar;
+
+  final AvataaarsApi api = AvataaarsApi();
+
+  String getUrl() {
+    return api.getUrl(avatar, widget.radius);
+  }
+
+  Top _getTop() {
+    if (widget.gender == null) {
+      return Top.random;
+    } else if (widget.gender == Gender.male) {
+      return Top.shortHairShortFlat(
+        facialHair: FacialHair.blank,
+      );
+    }
+    return Top.longHairStraightStrand(
+      facialHair: FacialHair.blank,
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    avatar = Avataaar(
+        top: _getTop(),
+        clothes: Clothes.random,
+        style: Style.random,
+        eyes: Eyes.random,
+        skin: Skin.random,
+        eyebrow: Eyebrow.random,
+        mouth: Mouth.random);
+  }
 
   @override
   Widget build(BuildContext context) {
     return CircleAvatar(
-      radius: radius,
-      backgroundColor: backgroundColor,
+      radius: widget.radius,
+      backgroundColor: widget.backgroundColor,
       child: AvataaarImage(
-        avatar: Avataaar.random(),
-        errorImage: errorImage,
-        placeholder: placeholder,
+        avatar: avatar,
+        errorImage: widget.errorImage,
+        placeholder: widget.placeholder,
       ),
     );
   }
